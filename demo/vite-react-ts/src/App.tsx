7145abe5a2368efcb01a770197282fcc8f001e09
Raul Melo <melo.raulf@gmail.com>
2022-04-02 13:26:05 +0200

    chore: add demo with vite

---
+++ demo/vite-react-ts/src/App.tsx
@@ -0,0 +1,41 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useRef } from "react";
+
+import { Resizable } from "../../../dist/my-lib.es";
+
+function App() {
+  const sidebarRef = useRef<HTMLDivElement>(null);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello Vite + React!</p>
+        <div className="sidePanel" ref={sidebarRef}>
+          <Resizable parentRef={sidebarRef} />
+        </div>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {" | "}
+          <a
+            className="App-link"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
